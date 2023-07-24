e355cc5dfde8a7c40e89b46d6513f028bb92cd5a
oddtO <121516833+oddtO@users.noreply.github.com>
2023-07-25 01:13:47 +0300

    add form display on click

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-
-
+(function () {
+  let newBookBtn = document.querySelector("button.add-new-book");
+  newBookBtn.addEventListener("click", () => {
+    let newBookForm = document.querySelector("form.add-new-book");
+    newBookForm.style.visibility = "visible";
+  });
+})();
